7db286ac5d2362c5859760f69c517a75f10eb83f
Alyiah <anprocto@uncg.edu>
2022-09-30 18:56:49 -0400

    Occupation Table Read

---
+++ app/routes.py
@@ -0,0 +1,33 @@
+#from flask import flask
+from flask import render_template
+from app import app
+import pandas as pd
+import numpy as np
+import pyodbc
+
+
+#app.config['TEMPLATES_AUTO_RELOAD'] = True
+@app.route('/')
+@app.route('/index')
+def index():
+
+    #connecting to the data base
+    server = 'statefinder.database.windows.net'
+    database= 'LivingWage'
+    username = 'statefinder'
+    password = '{FAll2022}'
+    driver = '{ODBC Driver 17 for SQL Server}'
+    conn = pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = conn.cursor()
+
+    #queries
+    q = ("SELECT * FROM Occupations;")
+
+    occ = pd.DataFrame()
+
+    #saving sql tables as panda dataframes
+    occ = pd.read_sql_query(q, conn)
+    conn.close
+    
+    #sending data to html files
+    return render_template('index.html', data = occ)
